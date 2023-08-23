b3c8a9535fe3d7ade1805f000b535b51e2d1fe29
Aditya Sharma <adihgr18@gmail.com>
2023-08-23 22:11:52 +0530

    profile picture upload added

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-
+import { Image } from 'cloudinary-react';
+import {v4} from "uuid";
 const Popup = ({ contact, onClose, onSave }) => {
   const [editedContact, setEditedContact] = useState({ ...contact });
+  const [uploadedImage, setUploadedImage] = useState('');
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -11,7 +13,39 @@ const Popup = ({ contact, onClose, onSave }) => {
     }));
   };
 
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const formData = new FormData();
+      formData.append('file', file);
+      formData.append('public_id', `weblar-contacts/${v4()}`);
+      formData.append('upload_preset', 'webalar-assignment');
+
+      
+      // Upload the image to Cloudinary
+      fetch('https://api.cloudinary.com/v1_1/dnhslyteh/image/upload', {
+        method: 'POST',
+        body: formData,
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          
+          setUploadedImage(data.secure_url);
+        })
+        .catch((error) => {
+          console.error('Error uploading image:', error);
+        });
+    }
+  };
+
   const handleSave = () => {
+    if (uploadedImage) {
+      setEditedContact((prevContact) => ({
+        ...prevContact,
+        profilePic: uploadedImage,
+      }));
+    }
+
     onSave(editedContact);
     onClose();
   };
@@ -30,7 +64,23 @@ const Popup = ({ contact, onClose, onSave }) => {
               alt={`${editedContact.name}'s Profile`}
               className="w-12 h-12 rounded-full object-cover mr-4"
             />):
-            (<button><img className="w-12 h-12 rounded-full object-cover mr-4" src="https://img.icons8.com/external-global-made-by-made/50/external-Uplaod-ui-and-ux-global-made-by-made.png" alt="external-Uplaod-ui-and-ux-global-made-by-made"/></button>)
+            (<><label htmlFor="profilePic" className="cursor-pointer">
+            <Image
+              cloudName="dnhslyteh"
+              publicId={uploadedImage || 'https://res.cloudinary.com/dnhslyteh/image/upload/v1692808798/cloud_upload_file_storage_upload_icon-1320190558968694328_jdsitl.png'|| "https://res.cloudinary.com/dnhslyteh/image/upload/v1692808859/male-profile-flat-blue-simple-icon-with-long-shadow_esfk41.jpg"}
+              width="48"
+              height="48"
+              crop="fill"
+              className="w-12 h-12 rounded-full object-cover mr-4"
+            />
+          </label>
+          <input
+            type="file"
+            id="profilePic"
+            accept="image/*"
+            className="hidden"
+            onChange={handleImageUpload}
+          /></>)
           }
           <div>
             <label className="block text-sm font-medium text-gray-600">
